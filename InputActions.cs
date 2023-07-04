951a99bfbff87818995918e0551278a3f2bcad44
Ackeresu <130289522+Ackeresu@users.noreply.github.com>
2023-07-04 15:45:00 +0200

    fixed jump animations, added run

--- InputActions.cs
+++ InputActions.cs
@@ -7,9 +7,11 @@ using UnityEngine;
 public class InputActions : MonoBehaviour {
 
     private GameInput gameInput; // Get the info of the auto-generated class that manage the inputs
-
+    
     public event EventHandler <Vector2> OnPlayerMovement;
+    public event EventHandler <bool> OnPlayerRun;
     public event EventHandler OnPlayerJump;
+    public event EventHandler OnPlayerDash;
 
     private void Awake() {
         gameInput = new GameInput();
@@ -18,7 +20,12 @@ public class InputActions : MonoBehaviour {
         gameInput.Player.Move.canceled += Move;
         gameInput.Player.Move.performed += Move;
 
+        gameInput.Player.Run.started += Run;
+        gameInput.Player.Run.canceled += Run;
+        gameInput.Player.Run.performed += Run;
+
         gameInput.Player.Jump.performed += JumpPerformed;
+        gameInput.Player.Dash.performed += DashPerformed;
 
         gameInput.Player.Enable();
     }
@@ -28,7 +35,16 @@ public class InputActions : MonoBehaviour {
         OnPlayerMovement?.Invoke(this, movement);
     }
 
+    private void Run(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        bool running = gameInput.Player.Run.IsPressed();
+        OnPlayerRun?.Invoke(this, running);
+    }
+
     private void JumpPerformed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
         OnPlayerJump?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void DashPerformed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+        OnPlayerDash?.Invoke(this, EventArgs.Empty);
     }
 }
--- Player.cs
+++ Player.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -10,22 +11,29 @@ public class Player : MonoBehaviour {
     [SerializeField] private BoxCollider2D standingCollider;
     [SerializeField] private BoxCollider2D jumpingCollider;
 
-    public float moveSpeed = 3f;
-    public float jumpForce = 2f;
-    public int maxAirJumps = 0;
+    public float moveSpeed = 2f;
+    public float runSpeed = 3f;
+    public float jumpHeight = 4f;
+    public float airJumpHeight = 3f;
+    public int maxAirJumps = 1;
+    public float dashLenght = 2f;
     
     private Rigidbody2D body;
     private Animator anim;
-    private float movementX;
+
+    private Vector2 playerMovement;
+    private float movementInputX;
+    //private float movementInputY;
     private int availableAirJumps;
 
     private CheckGroundedState groundedState;
     private bool shouldCheckGround;
+    private bool isRunning;
 
     // Animation variables
     private static string SPEED = "Speed";
     private static string IS_JUMPING = "isJumping";
-
+    //private static string IS_CROUCHING = "isCrouching";
 
     private void Start() {
         body = GetComponent<Rigidbody2D>();
@@ -35,12 +43,106 @@ public class Player : MonoBehaviour {
         availableAirJumps = maxAirJumps;
 
         input.OnPlayerMovement += HandleMovement;
+        input.OnPlayerRun += HandleRunning;
         input.OnPlayerJump += HandleJumping;
+        input.OnPlayerDash += HandleDashing;
     }
 
     private void Update() {
+        UpdateMovement();
         HandleStuff();
+        GroundCheck();
+    }
 
+    private void UpdateMovement() {
+        if (!isRunning) {
+            playerMovement = new Vector2(movementInputX * moveSpeed, body.velocity.y);
+        } else {
+            playerMovement = new Vector2(movementInputX * runSpeed, body.velocity.y);
+        }
+        body.velocity = playerMovement;
+
+        //body.gravityScale = (groundedState && Mathf.Approximately(movementX, 0)) ? 0 : 1;
+    }
+
+    private void HandleStuff() {
+        MovingPlatform platform = null;
+        if (groundedState != null) {
+            platform = groundedState.GetComponent<MovingPlatform>();
+        }
+        if (platform != null) {
+            transform.parent = platform.transform;
+        } else {
+            transform.parent = null;
+        }
+
+        // Player scale
+        Vector3 pScale = Vector3.one;
+        if (platform != null) {
+            pScale = platform.transform.localScale;
+        }
+        if (!Mathf.Approximately(playerMovement.x, 0)) {
+            transform.localScale = new Vector3(Mathf.Sign(-playerMovement.x) / pScale.x, 1 / pScale.y, 1);
+        }
+
+        // Animations
+        anim.SetFloat(SPEED, Mathf.Abs(playerMovement.x));
+
+        if (!Mathf.Approximately(playerMovement.x, 0)) {
+            transform.localScale = new Vector3(Mathf.Sign(-playerMovement.x), 1, 1);
+        }
+        //if (!Mathf.Approximately(0, movementY)) {
+        //    anim.SetBool(IS_CROUCHING, true);
+        //} else {
+        //    anim.SetBool(IS_CROUCHING, false);
+        //}
+    }
+
+    private void HandleMovement(object sender, Vector2 movementInput) {
+        movementInputX = movementInput.x;
+    }
+
+    private void HandleRunning(object sender, bool runningInput) {
+        if (runningInput) {
+            isRunning = true;
+        } else {
+            isRunning = false;
+        }
+    }
+
+    private void HandleJumping(object sender, EventArgs empty) {
+        if (groundedState.GetIsGrounded()) {
+            // Reset the Y momentum before applying the force
+            Vector2 newVelocity = new Vector2(body.velocity.x, 0);
+            body.velocity = newVelocity;
+            body.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+
+            standingCollider.enabled = false;
+            jumpingCollider.enabled = true;
+
+            anim.SetBool(IS_JUMPING, true);
+        }
+
+        if (!groundedState.GetIsGrounded() && availableAirJumps > 0) {
+            // Reset the Y momentum before applying the force
+            Vector2 newVelocity = new Vector2(body.velocity.x, 0);
+            body.velocity = newVelocity;
+            body.AddForce(Vector2.up * airJumpHeight, ForceMode2D.Impulse);
+
+            availableAirJumps -= 1;
+
+            anim.SetBool(IS_JUMPING, true);
+        }
+    }
+
+    private void HandleDashing(object sender, EventArgs empty) {
+        //if (!groundedState.GetIsGrounded()) {
+            Debug.Log("dashing");
+            //body.AddRelativeForce(Vector2.forward * dashLenght, ForceMode2D.Impulse);
+        //}
+    }
+
+    private void GroundCheck() {
         if (body.velocity.y <= 0) {
             shouldCheckGround = true;
         }
@@ -57,58 +159,10 @@ public class Player : MonoBehaviour {
         }
     }
 
-    private void HandleStuff() {
-        Vector2 movement = new Vector2(movementX, body.velocity.y);
-        body.velocity = movement;
-
-        MovingPlatform platform = null;
-        if (groundedState != null) {
-            platform = groundedState.GetComponent<MovingPlatform>();
-        }
-        if (platform != null) {
-            transform.parent = platform.transform;
-        } else {
-            transform.parent = null;
-        }
-
-        // Player scale
-        Vector3 pScale = Vector3.one;
-        if (platform != null) {
-            pScale = platform.transform.localScale;
-        }
-        if (!Mathf.Approximately(movementX, 0)) {
-            transform.localScale = new Vector3(Mathf.Sign(-movementX) / pScale.x, 1 / pScale.y, 1);
-        }
-
-        // Scale for animation
-        anim.SetFloat(SPEED, Mathf.Abs(movementX));
-        if (!Mathf.Approximately(movementX, 0)) {
-            transform.localScale = new Vector3(Mathf.Sign(-movementX), 1, 1);
-        }
-    }
-
-    private void HandleMovement(object sender, Vector2 inputMovement) {
-        movementX = inputMovement.x * moveSpeed;
-    }
-
-    private void HandleJumping(object sender, EventArgs empty) {
-        if (groundedState.GetIsGrounded()) {
-            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-
-            standingCollider.enabled = false;
-            jumpingCollider.enabled = true;
-
-            anim.SetBool(IS_JUMPING, true);
-        }
-
-        if (!groundedState.GetIsGrounded() && availableAirJumps > 0) {
-            body.AddForce(Vector2.up * (jumpForce / 2), ForceMode2D.Impulse);
-            availableAirJumps -= 1;
-        }
-    }
-
     private void OnDestroy() {
         input.OnPlayerMovement -= HandleMovement;
+        input.OnPlayerRun -= HandleRunning;
         input.OnPlayerJump -= HandleJumping;
+        input.OnPlayerDash -= HandleDashing;
     }
 }
